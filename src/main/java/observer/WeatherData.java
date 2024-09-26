3db28a45437e65842688966d1f41897f659511a7
anth <zyle1104@gmail.com>
2024-09-26 14:07:19 +0900

    ✨ Feat: add opserver pattern implementation

---
+++ src/main/java/observer/WeatherData.java
@@ -0,0 +1,63 @@
+package observer;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 옵저버를 리스트로 담는다.  ( 옵저버 될 객체들 추가, 삭제 가 가능해짐 )
+ * 웨더데이터는 옵저버를 등록, 삭제, 알림을 하는 메소드를 가진다.
+ * 생성자를 이용해 옵저버 리스트를 초기화한다.
+ * 옵저버들을 업데이트하는 메소드 생성
+ * 측정값 변경 메소드 생성
+ * 측정값 셋팅 후 측정값 변경 메소드 호출 ( 옵저버들에게 알림 )
+ * 나머지는 게터
+ */
+public class WeatherData implements Subject {
+	private List<Observer> observers;
+	private float temperature;
+	private float humidity;
+	private float pressure;
+
+	public WeatherData() {
+		observers = new ArrayList<Observer>();
+		System.out.println("\"찍어봐요?\" = " + "찍어봐요?");
+	}
+
+	public void registerObserver(Observer o) {
+		observers.add(o);
+	}
+
+	public void removeObserver(Observer o) {
+		observers.remove(o);
+	}
+
+	public void notifyObservers() {
+		for (Observer observer : observers) {
+			observer.update(temperature, humidity, pressure);
+		}
+	}
+
+	public void measurementsChanged() {
+		notifyObservers();
+	}
+
+	public void setMeasurements(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.pressure = pressure;
+		measurementsChanged();
+	}
+
+	public float getTemperature() {
+		return temperature;
+	}
+
+	public float getHumidity() {
+		return humidity;
+	}
+
+	public float getPressure() {
+		return pressure;
+	}
+
+}
---
+++ src/main/java/observer/display/CurrentConditionsDisplay.java
@@ -0,0 +1,36 @@
+package observer.display;
+
+import observer.DisplayElement;
+import observer.Observer;
+import observer.WeatherData;
+
+/**
+ * 온도와 습도를 표시하는 디스플레이
+ * Observer 인터페이스를 구현하고, WeatherData 객체에 등록한다.
+ * 디스플레이를 업데이트하면, 온도와 습도를 출력한다.
+ * DisplayElement는 디스플레이 메소드를 전부 디스플레이별로 가지고있다고 가정하고 생성.
+ */
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+	private float temperature;
+	private float humidity;
+	private WeatherData weatherData;
+	
+	public CurrentConditionsDisplay(WeatherData weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+	}
+	
+	public void update(float temperature, float humidity, float pressure) {
+//		this.temperature = temperature;
+//		this.humidity = humidity;
+		this.temperature = weatherData.getTemperature();
+		this.humidity = weatherData.getHumidity();
+		//이런식으로 하면 필요한것만 가져올수있음
+		display();
+	}
+	
+	public void display() {
+		System.out.println("Current conditions: " + temperature 
+			+ "F degrees and " + humidity + "% humidity");
+	}
+}
